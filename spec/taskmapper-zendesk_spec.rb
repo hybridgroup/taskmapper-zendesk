2d7faa8dff2b2b74430f59ad61812e562fe1ad25
Rafael George <george.rafael@gmail.com>
2012-05-11 18:42:57 +0000

    removed ticketmaster instance variables from taskmapper_spec

--- spec/taskmapper-zendesk_spec.rb
+++ spec/taskmapper-zendesk_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "TaskMapper::Provider::Zendesk" do
   before(:each) do 
-    @ticketmaster = TaskMapper.new(:zendesk, {:account => 'ticketmaster', :username => 'foo', :password => '000000'})
+    @taskmapper = TaskMapper.new(:zendesk, {:account => 'taskmapper', :username => 'foo', :password => '000000'})
     headers = {'Authorization' => 'Basic Zm9vOjAwMDAwMA==','Accept' => 'application/json'}
     ActiveResource::HttpMock.respond_to do |mock|
       mock.get '/search.json?query=status%3Aopen', headers, fixture_for('tickets', 'json'), 200
@@ -11,12 +11,12 @@ describe "TaskMapper::Provider::Zendesk" do
   end
 
   it "should be able to instantiate a new instance" do
-    @ticketmaster.should be_an_instance_of(TaskMapper)
-    @ticketmaster.should be_a_kind_of(TaskMapper::Provider::Zendesk)
+    @taskmapper.should be_an_instance_of(TaskMapper)
+    @taskmapper.should be_a_kind_of(TaskMapper::Provider::Zendesk)
   end
 
   it "should be true with valid authentication" do 
-    @ticketmaster.valid?.should be_true
+    @taskmapper.valid?.should be_true
   end
 
 end
